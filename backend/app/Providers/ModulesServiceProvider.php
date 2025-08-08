f074c6b103fe41ee6a3f8b522f2f6c6c1c8100c9
Oleh Mordach <omordach@gmail.com>
2025-08-08 11:38:39 +0200

    Register Events module

--- backend/app/Providers/ModulesServiceProvider.php
+++ backend/app/Providers/ModulesServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Modules\Core\Providers\CoreServiceProvider;
+use Modules\Events\Providers\EventsServiceProvider;
 use Modules\Membership\Providers\MembershipServiceProvider;
 use Modules\Grievances\Providers\GrievancesServiceProvider;
 use Modules\Payments\Providers\PaymentsServiceProvider;
@@ -13,6 +14,7 @@ class ModulesServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->register(CoreServiceProvider::class);
+        $this->app->register(EventsServiceProvider::class);
         $this->app->register(MembershipServiceProvider::class);
         $this->app->register(GrievancesServiceProvider::class);
         $this->app->register(PaymentsServiceProvider::class);
