d12c9226554c0e1644c8909704a14e072bcdff0e
Oleh Mordach <omordach@gmail.com>
2025-08-09 11:34:31 +0200

    Avoid guard recursion

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -2,7 +2,7 @@
 
 return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1')),
-    'guard' => ['sanctum'],
+    'guard' => ['web'],
     'expiration' => null,
     'middleware' => [
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
